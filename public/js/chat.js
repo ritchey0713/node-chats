3ebfee42fdae7237f3dd1c674c30b1f150154aaa
Dj <ritchey0713@gmail.com>
2020-02-16 12:46:14 -0500

    add listener

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,3 +3,10 @@ const socket = io()
 socket.on("countUpdated", (count) => {
   console.log("The count has been updated", count)
 })
+
+const increment = document.querySelector("#increment")
+
+increment.addEventListener("click", () => {
+  console.log("clicked")
+  socket.emit("increment")
+})
