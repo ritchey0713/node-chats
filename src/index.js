07a3f78f24e7351ec42342b187d9a8f2cc89b044
Dj <ritchey0713@gmail.com>
2020-02-16 12:28:38 -0500

    set up server socket

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const path = require("path")
 const express = require('express')
+const http = require('http')
+const socketio = require("socket.io")
+
 const app = express()
+// create server to pass to socketio
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 
@@ -8,6 +14,15 @@ const dirPath = path.join(__dirname, '../public')
 
 app.use(express.static(dirPath))
 
-app.listen(port, () => {
+let count = 0
+
+io.on("connection", (socket) => {
+  console.log("New web socket connection")
+
+  socket.emit("countUpdated")
+})
+
+
+server.listen(port, () => {
   console.log(`Server loaded at ${port}`)
 })
