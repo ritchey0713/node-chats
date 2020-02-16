8a6a15f3372bfabb1e7c6f563691b4295abc8f90
Dj <ritchey0713@gmail.com>
2020-02-16 13:54:37 -0500

    change naming

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,17 +1,17 @@
 const socket = io()
 
-const submit = document.querySelector("form")
+const form = document.querySelector("form")
 
 socket.on("welcomeMessage", (message) => {
   console.log(message)
 })
 
-socket.on("sendMessage", (message) => {
-  console.log("New message:", message)
-})
-
-submit.addEventListener("submit", (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault()
   const message = e.target.elements.chatBox.value
   socket.emit("sendMessage", message)
+})
+
+socket.on("sendMessage", (message) => {
+  console.log("New message:", message)
 })
