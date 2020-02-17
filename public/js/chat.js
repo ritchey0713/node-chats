11ca0ff9e6a4fa46f73d0e271d02b61e28d84a48
Dj <ritchey0713@gmail.com>
2020-02-17 13:28:07 -0500

    pass createdAt to template

--- public/js/chat.js
+++ public/js/chat.js
@@ -15,7 +15,8 @@ socket.on("message", (message) => {
   console.log(message)
 
   const html = Mustache.render(messageTemplate, {
-    message: message.text
+    message: message.text,
+    createdAt: message.createdAt
   })
   messages.insertAdjacentHTML("beforeend", html)
 })
