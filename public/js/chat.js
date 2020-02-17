737cc5e6843f3a765f3d17e03c83fb49eaf22266
Dj <ritchey0713@gmail.com>
2020-02-17 13:24:28 -0500

    call on text from msg

--- public/js/chat.js
+++ public/js/chat.js
@@ -15,7 +15,7 @@ socket.on("message", (message) => {
   console.log(message)
 
   const html = Mustache.render(messageTemplate, {
-    message
+    message: message.text
   })
   messages.insertAdjacentHTML("beforeend", html)
 })
