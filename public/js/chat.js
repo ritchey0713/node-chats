e3cf5f83fc30193103efa30cc580ee6f1c099ac2
Dj <ritchey0713@gmail.com>
2020-02-17 15:51:42 -0500

    set up query for names and rooms

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,6 +11,9 @@ const messages = document.querySelector("#messages")
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationTemplate = document.querySelector("#location-template").innerHTML
 
+//Options 
+const { username, room } = Qs.parse(location.parse, { ignoreQueryPrefix: true })
+
 socket.on("message", (message) => {
   const html = Mustache.render(messageTemplate, {
     message: message.text,
@@ -69,6 +72,6 @@ userLocation.addEventListener("click", (e) => {
   })
 })
 
-
+socket.emit("join", { username, room })
 
 
